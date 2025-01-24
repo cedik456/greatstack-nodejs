f4a0d83f790a092be08551d0409e7daba3063845
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-24 19:27:13 +0800

    Created a simple http server using node js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 
 // fs.readFile("example.txt", "utf8", (err, data) => {
 //   if (err) {
@@ -8,12 +9,26 @@ const fs = require("fs");
 //   console.log(data);
 // });
 
-const content = "Some content!";
+// --------------------------------------------
 
-fs.writeFile("output.txt", content, (err) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log("File created");
-  return;
+// const content = "Some content!";
+
+// fs.writeFile("output.txt", content, (err) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log("File created");
+//   return;
+// });
+
+// ------------------ Server -----------------------
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
