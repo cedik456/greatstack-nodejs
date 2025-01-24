6e6cfa2258d24605ed1239e6fdff956e98e218d1
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-24 18:47:04 +0800

    Learned how to write file using nodeJS file system

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
-fs.readFile("example.txt", "utf8", (err, data) => {
+// fs.readFile("example.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const content = "Some content!";
+
+fs.writeFile("output.txt", content, (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log(data);
+  console.log("File created");
+  return;
 });
