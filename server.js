a1de8ea79f2a0d7278d9aeb0df1f08220305ca2f
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-27 17:17:45 +0800

    Learn how to handle query in node js using the http module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // GET REQUEST
 
 const http = require("http");
+const url = require("url");
 
 // const server = http.createServer((req, res) => {
 //   if (req.method === "GET" && req.url === "/") {
@@ -20,18 +21,38 @@ const http = require("http");
 
 // POST REQUEST
 
+// const server = http.createServer((req, res) => {
+//   if (req.method === "POST" && req.url === "/submit") {
+//     let body = "";
+
+//     req.on("data", (chunk) => {
+//       body += chunk.toString();
+//     });
+
+//     req.on("end", () => {
+//       res.writeHead(200, { "content-type": "application/json" });
+//       res.end(JSON.stringify({ message: "Data Received", data: body }));
+//     });
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Route not Found");
+//   }
+// });
+
+// const PORT = 4000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// HANDLE QUERY
+
 const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/submit") {
-    let body = "";
+  if (req.method === "GET" && req.url.startsWith("/search")) {
+    const queryObject = url.parse(req.url, true).query;
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "Data Received", data: body }));
-    });
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Query Received:", queryObject }));
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
     res.end("Route not Found");
