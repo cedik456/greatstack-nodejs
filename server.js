1f28c02db73a2bbd98354ff9fc162d7f07d90ff7
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-27 15:52:57 +0800

    Learn how to handle POST requests in node js

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
+// GET REQUEST
+
 const http = require("http");
 
+// const server = http.createServer((req, res) => {
+//   if (req.method === "GET" && req.url === "/") {
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("Welcome to homepage");
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Page not found");
+//   }
+// });
+
+// const PORT = 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// POST REQUEST
+
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Welcome to homepage");
+  if (req.method === "POST" && req.url === "/submit") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Data Received", data: body }));
+    });
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
-    res.end("Page not found");
+    res.end("Route not Found");
   }
 });
 
-const PORT = 3000;
+const PORT = 4000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
