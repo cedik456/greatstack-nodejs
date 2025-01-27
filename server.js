0f5a264e2428ed829a004723ef24615ec9a60e7c
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-27 18:56:00 +0800

    Learn how to use routes in node js

--- server.js
+++ server.js
@@ -69,3 +69,36 @@ const url = require("url");
 // server.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
+
+// Route Handlers
+
+const routes = {
+  "/": (req, res) => {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Welcome to the Homepage");
+  },
+  "/about": (req, res) => {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("This is the about page");
+  },
+  "/notFound": (req, res) => {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("Page not Found");
+  },
+};
+
+const server = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+
+  if (routes[pathname]) {
+    routes[pathname](req, res);
+  } else {
+    routes["/notFound"](req, res);
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
