7105c5da202fefbbd4449f6210b97adaeb1aee28
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-31 23:42:29 +0800

    Learn how to use event emitters in node js

--- server.js
+++ server.js
@@ -179,24 +179,34 @@ const queryString = require("querystring");
 
 // HANDLING ENCODED URL DATA
 
-const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/submit") {
-    let data = "";
+// const server = http.createServer((req, res) => {
+//   if (req.method === "POST" && req.url === "/submit") {
+//     let data = "";
 
-    req.on("data", (chunk) => {
-      data += chunk;
-    });
+//     req.on("data", (chunk) => {
+//       data += chunk;
+//     });
 
-    req.on("end", () => {
-      const parsedData = queryString.parse(data);
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "Form data received:", parsedData }));
-    });
-  }
+//     req.on("end", () => {
+//       const parsedData = queryString.parse(data);
+//       res.writeHead(200, { "content-type": "application/json" });
+//       res.end(JSON.stringify({ message: "Form data received:", parsedData }));
+//     });
+//   }
+// });
+
+// const PORT = 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+emitter.on("log", () => {
+  console.log("Hello World!");
 });
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+emitter.emit("log");
